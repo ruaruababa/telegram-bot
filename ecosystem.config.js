b5eae8b71e6ba08e3d9ffb73f3135c5ff980e5b4
Yang <dethia1toanlyanh@gmail.com>
2024-03-13 11:29:21 +0700

    Add log files to .gitignore and update script in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,28 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'Telegram Bot',
-      script: 'index.js',
-      watch: '.',
+      script: 'tsc --watch "*.ts" --exec "ts-node" index.ts',
       out_file: './out.log',
       error_file: './error.log',
     },
-    {
-      script: './service-worker/',
-      watch: ['./service-worker'],
-    },
+    // {
+    //   script: './service-worker/',
+    //   watch: ['./service-worker'],
+    // },
   ],
-
-  deploy: {
-    production: {
-      user: 'SSH_USERNAME',
-      host: 'SSH_HOSTMACHINE',
-      ref: 'origin/master',
-      repo: 'GIT_REPOSITORY',
-      path: 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy':
-        'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-    },
-  },
 };
